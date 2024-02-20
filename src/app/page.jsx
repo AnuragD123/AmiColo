537803a72193a1b72d64e94fcded2d98f5ea4cd0
VarunSharma3250 <av279508@gmail.com>
2024-02-20 19:37:30 +0530

    fix: made it responsive

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,25 +3,32 @@ import Image from "next/image";
 export default function Home() {
     return (
         <main className="">
-            <div id="room-friend-sitting-photo-and-maple-leaf" className="relative">
-    <p className="z-10 absolute left-[80rem] top-36 text-4xl">
-        Your <span className="text-red-600 ">New City</span>, <br />
-        Your <span className="text-blue-600">New Home</span>.
-    </p>
-    <img
-        className="w-[100vw] z-20"
-        src="/images/friends.png"
-        alt="friends sitting in a room"
-    />
-    <img
-        className="w-[19vw] z-10 absolute top-[64.5rem] left-[30.5rem]"
-        src="/images/maple.png"
-        alt="canadian-maple-leaf"
-    />
-</div>
-<div id="section-2" className="relative z-30 mb-16">
+            <div
+                id="room-friend-sitting-photo-and-maple-leaf"
+                className="relative"
+            >
+                <p className="hidden lg:inline z-10 absolute left-[80rem] top-36   md:text-4xl">
+                    Your <span className="text-red-600 ">New City</span>, <br />
+                    Your <span className="text-blue-600">New Home</span>.
+                </p>
+                <img
+                    className="w-[100vw] z-20"
+                    src="/images/friends.png"
+                    alt="friends sitting in a room"
+                />
+                <img
+                    className="hidden lg:inline w-[19vw] z-10 absolute top-[64rem] left-[30.5rem]"
+                    src="/images/maple.png"
+                    alt="canadian-maple-leaf"
+                />
+            </div>
+            <div
+                id="section-2"
+                className="relative z-30 mb-16"
+            >
                 <p className="text-4xl mb-2 font-semibold text-center">
-                    We are <span className="text-white">Canada’s mo</span>st Awaited Coiling Platform
+                    We are <span className="lg:text-white">Canada’s mo</span>st
+                    Awaited Coiling Platform
                 </p>
                 <div className="plus-card-content flex justify-evenly">
                     <div className="plus-card">
@@ -44,64 +51,54 @@ export default function Home() {
                     </div>
                 </div>
             </div>
-            <div
-                id="section-3"
-                className="flex justify-between content-center"
-            >
-                <img
-                    className="w-[50vw]"
-                    src="/images/find-easy.png"
-                    alt="find-and-easy"
-                />
-                <div className="content m-auto">
-                    <h5 className="heading text-xl text-start mx-8 mb-3">
-                        Kind to Your Wallet,
-                        <br />
-                        <span className="font-bold text-2xl">
-                            Gentle on Your Thoughts
-                        </span>
-                    </h5>
-                    <div className="content-p flex justify-around">
-                        <div className="vertical-heading text-start text-lg">
-                            <p className="my-3 mx-8">
-                                AI Powered Roommate
-                                <br />
-                                <span>Recommendations</span>
-                            </p>
-                            <p className="my-3 mx-8">
-                                Housekeeping <br />
-                                <span>Amenities</span>
-                            </p>
-                        </div>
-                        <div className="vertical-heading text-start text-lg">
-                            <p className="my-3 mx-8">
-                                AI Powered
-                                <br />
-                                <span>24 x 7 Support</span>
-                            </p>
-                            <p className="my-3 mx-8">
-                                Zero
-                                <br />
-                                <span>Brokerage</span>
-                            </p>
-                        </div>
-                        <div className="vertical-heading text-start text-lg">
-                            <p className="my-3 mx-8">
-                                AmiColo Community
-                                <br />
-                                <span>Recommendations</span>
-                            </p>
-                            <p className="my-3 mx-8">
-                                Housekeeping <br />
-                                <span>Membership</span>
-                            </p>
-                        </div>
+            <div className="content m-auto">
+                <h5 className="heading text-xl text-start mx-8 mb-3">
+                    Kind to Your Wallet,
+                    <br />
+                    <span className="font-bold text-2xl">
+                        Gentle on Your Thoughts
+                    </span>
+                </h5>
+                <div className="content-p flex justify-around">
+                    <div className="vertical-heading text-start text-xs lg:text-lg">
+                        <p className="my-3 mx-2 lg:mx-8">
+                            AI Powered Roommate
+                            <br />
+                            <span>Recommendations</span>
+                        </p>
+                        <p className="my-3 mx-2 lg:mx-8">
+                            Housekeeping <br />
+                            <span>Amenities</span>
+                        </p>
+                    </div>
+                    <div className="vertical-heading text-start text-xs lg:text-lg">
+                        <p className="my-3 mx-2 lg:mx-8">
+                            AI Powered
+                            <br />
+                            <span>24 x 7 Support</span>
+                        </p>
+                        <p className="my-3 mx-2 lg:mx-8">
+                            Zero
+                            <br />
+                            <span>Brokerage</span>
+                        </p>
+                    </div>
+                    <div className="vertical-heading text-start text-xs lg:text-lg">
+                        <p className="my-3 mx-2 lg:mx-8">
+                            AmiColo Community
+                            <br />
+                            <span>Recommendations</span>
+                        </p>
+                        <p className="my-3 mx-2 lg:mx-8">
+                            Housekeeping <br />
+                            <span>Membership</span>
+                        </p>
                     </div>
                 </div>
             </div>
             <div
                 id="section-4"
-                className="flex justify-between content-center"
+                className="flex flex-col-reverse lg:flex-row justify-between content-center"
             >
                 <div className="content m-auto">
                     <h5 className="heading text-xl text-start mx-8 mb-3">
@@ -113,11 +110,11 @@ export default function Home() {
                     </h5>
                     <div className="content-p flex justify-around">
                         <div className="vertical-heading text-start text-lg">
-                            <p className="my-3 mx-8">
+                            <p className="my-3 mx-2 lg:mx-8">
                                 Personalized <br />
                                 <span>Matching</span>
                             </p>
-                            <p className="my-3 mx-8">
+                            <p className="my-3 mx-2 lg:mx-8">
                                 Clean <br />
                                 <span>Environment</span>
                             </p>
@@ -133,7 +130,7 @@ export default function Home() {
                             </p>
                         </div>
                         <div className="vertical-heading">
-                            <p className="my-3 mx-8">
+                            <p className="my-3 mx-2 lg:mx-8">
                                 Hassle Free <br />
                                 <span>Payments</span>
                             </p>
@@ -141,17 +138,17 @@ export default function Home() {
                     </div>
                 </div>
                 <img
-                    className="w-[50vw]"
+                    className="w-[100vw] lg:w-[50vw]"
                     src="/images/stay-easy.png"
                     alt="stay-easy"
                 />
             </div>
             <div
                 id="section-5"
-                className="flex justify-between content-center"
+                className="flex flex-col lg:flex-row justify-between content-center"
             >
                 <img
-                    className="w-[50vw]"
+                    className="w-[100vw] lg:w-[50vw]"
                     src="/images/bond-easy.png"
                     alt="bond-easy"
                 />
@@ -164,32 +161,32 @@ export default function Home() {
                     </h5>
                     <div className="content-p flex justify-around">
                         <div className="vertical-heading text-start text-lg">
-                            <p className="my-3 mx-8">
+                            <p className="my-3 mx-2 lg:mx-8">
                                 Choose Your <br />
                                 <span>Coving Mates</span>
                             </p>
-                            <p className="my-3 mx-8">
+                            <p className="my-3 mx-2 lg:mx-8">
                                 Events, Celebrations <br />
                                 <span>& Pop Ups</span>
                             </p>
                         </div>
                         <div className="vertical-heading text-start text-lg">
-                            <p className="my-3 mx-8">
+                            <p className="my-3 mx-2 lg:mx-8">
                                 Social <br />
                                 <span>Calendar</span>
                             </p>
-                            <p className="my-3 mx-8">
+                            <p className="my-3 mx-2 lg:mx-8">
                                 Network &
                                 <br />
                                 <span>Collaborate</span>
                             </p>
                         </div>
                         <div className="vertical-heading text-start text-lg">
-                            <p className="my-3 mx-8">
+                            <p className="my-3 mx-2 lg:mx-8">
                                 Choose Your <br />
                                 <span>Coving Mates</span>
                             </p>
-                            <p className="my-3 mx-8">
+                            <p className="my-3 mx-2 lg:mx-8">
                                 Career
                                 <br />
                                 <span>Fairs</span>
@@ -209,10 +206,10 @@ export default function Home() {
                 />
                 <div className="content text-center my-auto">
                     <div className="flex flex-col justify-center items-center">
-                        <h5 className="text-4xl lg:text-7xl font-bold">
+                        <h5 className="text-md md:text-4xl lg:text-7xl font-bold">
                             Let’s make some
                         </h5>
-                        <h5 className="text-6xl lg:text-9xl font-bold">
+                        <h5 className="text-sm md:text-6xl lg:text-9xl font-bold">
                             Memories!
                         </h5>
                         <button className="text-white py-2 px-6 lg:px-8 mt-4 lg:mt-6 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-orange-400">
