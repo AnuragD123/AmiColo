194cdcde307ed37641139f77a2a00e3ce95b82a3
Sidu52 <hitechsidu992@gmail.com>
2024-02-20 19:16:14 +0530

    Add Profile Sections Fields

---
+++ src/app/profile/orderreceiept/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OrderReceipet = () => {
+    return (
+        <div className=' flex-1 p-2 flex flex-col items-center'>
+            <p className=' w-4/5 text-center text-3xl text-white font-extrabold py-3' style={{ backgroundImage: "linear-gradient(#EF8463, #7170F5 )", clipPath: "polygon(3% 0%, 97% 0%, 100% 50%, 97% 100%, 3% 100%, 0% 50%)" }}>Order Receipts</p>
+
+        </div >
+    )
+}
+
+export default OrderReceipet
