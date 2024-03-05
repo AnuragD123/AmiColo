c1fc0e1ab8163817e6b0e5606d018895d29c445b
Anurag Dubey <anuragdubey859@gmail.com>
2024-03-05 22:57:29 +0530

    some minor changes

--- src/app/find_match/page.jsx
+++ src/app/find_match/page.jsx
@@ -28,7 +28,7 @@ const FindMatch = () => {
         <>
             <Toaster />
             <div className=' flex-1 p-2 flex flex-col items-center'>
-                <p className=' w-4/5 text-center text-3xl text-white font-extrabold py-3' style={{ backgroundImage: "linear-gradient(#EF8463, #7170F5 )", clipPath: "polygon(3% 0%, 97% 0%, 100% 50%, 97% 100%, 3% 100%, 0% 50%)" }}>My Roommates</p>
+                <p className=' w-4/5 text-center text-3xl text-white font-extrabold py-3' style={{ backgroundImage: "linear-gradient(#EF8463, #7170F5 )", clipPath: "polygon(3% 0%, 97% 0%, 100% 50%, 97% 100%, 3% 100%, 0% 50%)" }}>Find Matches</p>
                 <div className="mt-5 w-4/5 grid grid-cols-2 md:grid-cols-3 gap-4 justify-center items-center">
                     {
                         users.map((user) => (
