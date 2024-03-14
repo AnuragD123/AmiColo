7332768e0d01e4396a1a8510eeba97720bfb0894
m3rashid <m3rashid.hussain@gmail.com>
2024-03-14 19:55:10 +0530

    update

---
+++ src/app/api/user/api2/route.js
@@ -0,0 +1,12 @@
+import { getDataFromToken } from "@/helper/getDataFromToken";
+import { NextResponse } from "next/server";
+
+export async function POST(request) {
+	const userId = getDataFromToken(request)
+	console.log({ userId })
+
+	const response = await axios.post('http://89.116.49.229:5000/api2', { id });
+	console.log(response.data);
+
+	return NextResponse.json({ success: true, data: response.data })
+}
