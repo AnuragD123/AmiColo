51ac453155357b533e2c4c1320ae5501e7aad228
anzal1 <cofclan145@gmail.com>
2024-04-10 23:02:10 +0530

    updated the new stuff

--- src/helper/index.js
+++ src/helper/index.js
@@ -2,7 +2,7 @@ export const handleSanitizeData = (data) => {
   // replace all the null values with not available
   for (let key in data) {
     if (data[key] === null) {
-      data[key] = "Not Available";
+      data[key] = "Not Set";
     }
   }
   return data;
